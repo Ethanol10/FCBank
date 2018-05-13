312f48e5f7bf4f239dc08238e77c2d016514a6f8
Ethanol10 <35375774+Ethanol10@users.noreply.github.com>
2018-05-13 13:44:34 +1000

    Initial Commit.

---
+++ bank.c
@@ -0,0 +1,232 @@
+/*******************************************************************************
+ * 48430 Fundamentals of C Programming - Assignment 3
+ * Group:26
+ * Authors : 
+   Mohamad Win - 98034628
+   Ethan Goh - 12903151
+   Emmanuel Tshuma - 12311697
+   Varun Sriram - 12568731
+   Ngoc Thao Han Ho - 12868636
+ * Date of submission: 10/05/2018
+*******************************************************************************/
+
+/*******************************************************************************
+ * List of Header files. As the project is being developed, more header files 
+ may be added.
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+/*******************************************************************************
+ * List structs - you may define struct date and struct student only. As the 
+ project is being developed, more header files may be added.
+*******************************************************************************/
+struct account
+{
+	int id;
+	int pin;
+	double balance;	
+};
+typedef struct account account_t;
+
+struct jointAccount
+{
+	int userOneID;
+	int userOnePin;
+	int userTwoID;
+	int userTwoPin;
+	double balance;
+	
+};
+typedef struct account account_t;
+
+/*******************************************************************************
+ * Function prototypes. As the project is being developed, more function 
+ declarations may be added.
+*******************************************************************************/
+void addAccount(); 
+void editAccount(); 
+void removeAccount(); 
+void addJointAccount();
+void withdraw();
+void deposit();
+void transfer();
+void encrypt();
+void decrypt();
+void compress();
+void decompress();
+void printMenu(void);
+
+int main(void)
+{
+    return 0;     
+}
+
+/*******************************************************************************
+ * This function prints the initial menu with a list of functions the user may
+ choose to use
+ * - none
+ * outputs:
+ * - a new account
+ Author: Mohamad Win
+*******************************************************************************/
+void printMenu(void)
+{
+	
+}
+
+/*******************************************************************************
+ * This function adds an account to the system based on user input.
+ * inputs:
+ * - user details
+ * outputs:
+ * - a new account
+ Author: Varun Sriram
+*******************************************************************************/
+void addAccount()
+{
+
+}
+
+/*******************************************************************************
+ * This function allows the user to edit an account to the system based on user 
+ input.
+ * inputs:
+ * - account details 
+ * outputs:
+ * - edited account
+ Author: Emmanuel Tshuma
+*******************************************************************************/
+void editAccount()
+{
+
+}
+
+/*******************************************************************************
+ * This function allows the user to remove an account if they can provide 
+ required credentials.
+ * inputs:
+ * - account ID
+ * outputs:
+ * none
+ Author: Ngoc Thao Han Ho
+*******************************************************************************/
+void removeAccount()
+{
+
+}
+
+/*******************************************************************************
+ * This function allows the user to make a new Joint account.
+ * inputs:
+ * user details
+ * outputs:
+ * - a new account(joint)
+ Author: Ethan Goh
+*******************************************************************************/
+void JointAccount()
+{
+
+}
+
+/*******************************************************************************
+ * This function allows the user to withdraw money from their account 
+ input.
+ * inputs:
+ * - amount the user wishes to withdraw
+ * outputs:
+ * - none
+ Author: Emmanuel Tshuma
+*******************************************************************************/
+void withdraw()
+{
+	
+
+}
+
+/*******************************************************************************
+ * This function allows the user to deposit money into their account.
+ * inputs:
+ * - amount the user wishes to deposit
+ * outputs:
+ * - none
+ Author: Ngoc Thao Han Ho
+*******************************************************************************/
+void deposit()
+{
+	
+}
+
+/*******************************************************************************
+ * This function allows the user to transfer money from their account to 
+ another account. 
+ input.
+ * inputs:
+ * - account ID, account ID
+ * outputs:
+ * - none
+ Author: Mohamad Win
+*******************************************************************************/
+void transfer()
+{
+	
+}
+
+/*******************************************************************************
+ * This function encrypts the user's data 
+ input.
+ * inputs:
+ * - none
+ * outputs:
+ * - none
+ Author: Mohamad Win
+*******************************************************************************/
+void encrypt()
+{
+
+}
+
+/*******************************************************************************
+ * This function decrypts the user's data if they are able to provide correct 
+ credentials
+ input.
+ * inputs:
+ * - none
+ * outputs:
+ * - none
+ Author: Ethan Goh
+*******************************************************************************/
+void decrypt()
+{
+
+}
+
+/*******************************************************************************
+ * This function compresses the user's data 
+ input.
+ * inputs:
+ * - none
+ * outputs:
+ * - none
+ Author: Ethan Goh
+*******************************************************************************/
+void compress()
+{
+
+}
+
+/*******************************************************************************
+ * This function decompresses the user's data 
+ input.
+ * inputs:
+ * - none
+ * outputs:
+ * - none
+ Author: Varun Sriram
+*******************************************************************************/
+void decompress()
+{
+
+}
